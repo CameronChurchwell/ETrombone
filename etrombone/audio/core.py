bd7e25b73699eaa0ac4c00932d1ae5dc10e8b3ba
CameronChurchwell <steamtrains@icloud.com>
2023-05-09 11:53:25 -0500

    progress on audio processing

--- etrombone/audio/core.py
+++ etrombone/audio/core.py
@@ -3,7 +3,14 @@ Core audio processing file
 """
 
 from pedalboard import Pedalboard, Chorus, Compressor, Delay, Gain, Reverb, Phaser
+from pedalboard._pedalboard import load_plugin
 from pedalboard.io import AudioStream
+from pathlib import Path
+
+audio_dir = Path(__file__).parent
+
+brass_path = audio_dir / 'assets' / 'Aeternus Brass.vst3'
+brass = load_plugin(str(brass_path))
 
 # Open up an audio stream:
 with AudioStream(
@@ -12,22 +19,16 @@ with AudioStream(
     allow_feedback=True
 ) as stream:
     # Audio is now streaming through this pedalboard and out of your speakers!
-    stream.plugins = Pedalboard([
-        Compressor(threshold_db=-50, ratio=25),
-        Gain(gain_db=30),
-        Chorus(),
-        Phaser(),
-        Reverb(room_size=0.25),
-    ])
     while True:
         try:
-            reverb = input("enter reverb value [0-1]: ")
+            # reverb = input("enter reverb value [0-1]: ")
             stream.plugins = Pedalboard([
-                Compressor(threshold_db=-50, ratio=25),
-                Gain(gain_db=30),
-                Chorus(),
-                Phaser(),
-                Reverb(room_size=float(reverb)),
+                # Compressor(threshold_db=-50, ratio=25),
+                # Gain(gain_db=30),
+                # Chorus(),
+                # Phaser(),
+                # Reverb(room_size=float(reverb)),
+                brass
             ])
         except KeyboardInterrupt:
             print('done!')
